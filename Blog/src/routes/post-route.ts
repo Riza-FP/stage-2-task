bc3a8b356fc9d5a25579c591ed970e724b26bc8c
Riza Fauzan Pratama <rizafauzanpratama123@gmail.com>
2026-01-05 19:19:20 +0700

    feat: Add Day 1 Checkpoint 1

---
+++ Blog/src/routes/post-route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { getPosts, createPost, deletePost } from "../controllers/post-controller";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.post("/", createPost);
+router.delete("/:id", deletePost);
+
+
+export default router;
